685089aefb802735471488d63811e42db1ebdbe0
gissehel <public-devgit-dantus@gissehel.org>
2018-06-04 08:14:37 +0200

    Update docker API

--- dikkilib/raw_docker.py
+++ dikkilib/raw_docker.py
@@ -12,7 +12,8 @@ class RawDocker(object):
 
     def get_docker(self):
         if self._docker is None:
-            self._docker = docker.Client(base_url='unix://var/run/docker.sock', version='1.13', timeout=10)
+            # self._docker = docker.Client(base_url='unix://var/run/docker.sock', version='1.13', timeout=10)
+            self._docker = docker.APIClient(base_url='unix://var/run/docker.sock', version='1.21', timeout=10)
         return self._docker
 
     def get_images(self):
